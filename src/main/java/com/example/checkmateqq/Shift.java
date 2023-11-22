2ce81eebd0eef0738d1bcd714ded386bde47b195
veronikaAngelovic <vangelovicova@gmail.com>
2023-11-22 14:47:55 +0100

    shift and test class

---
+++ src/main/java/com/example/checkmateqq/Shift.java
@@ -0,0 +1,41 @@
+package com.example.checkmateqq;
+
+import java.util.Date;
+
+public class Shift {
+    int id;
+
+    int station_id;
+
+    Date date;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getStation_id() {
+        return station_id;
+    }
+
+    public void setStation_id(int station_id) {
+        this.station_id = station_id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Shift(int id, int station_id, Date date) {
+        this.id = id;
+        this.station_id = station_id;
+        this.date = date;
+    }
+}
---
+++ src/main/java/com/example/checkmateqq/Test.java
@@ -0,0 +1,61 @@
+package com.example.checkmateqq;
+
+public class Test {
+    int id;
+
+    int result;
+
+    String testcol;
+
+    int patient_id;
+
+    int shift_id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getResult() {
+        return result;
+    }
+
+    public void setResult(int result) {
+        this.result = result;
+    }
+
+    public String getTestcol() {
+        return testcol;
+    }
+
+    public void setTestcol(String testcol) {
+        this.testcol = testcol;
+    }
+
+    public int getPatient_id() {
+        return patient_id;
+    }
+
+    public void setPatient_id(int patient_id) {
+        this.patient_id = patient_id;
+    }
+
+    public int getShift_id() {
+        return shift_id;
+    }
+
+    public void setShift_id(int shift_id) {
+        this.shift_id = shift_id;
+    }
+
+    public Test(int id, int result, String testcol, int patient_id, int shift_id) {
+        this.id = id;
+        this.result = result;
+        this.testcol = testcol;
+        this.patient_id = patient_id;
+        this.shift_id = shift_id;
+    }
+}
