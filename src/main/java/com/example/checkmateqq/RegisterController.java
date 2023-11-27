003cda6dff86077d9d1cc4f1e4c8d09e5083db94
Bohdan <titankb@gmail.com>
2023-11-27 11:53:31 +0100

    version 2

--- src/main/java/com/example/checkmateqq/RegisterController.java
+++ src/main/java/com/example/checkmateqq/RegisterController.java
@@ -1,10 +1,19 @@
 package com.example.checkmateqq;
 
 import javafx.event.ActionEvent;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import java.util.function.UnaryOperator;
+import java.util.regex.Pattern;
+import javafx.scene.control.TextFormatter;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -14,6 +23,9 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class RegisterController {
+
+
+
 
     @FXML
     private PasswordField confirmPassword;
@@ -36,9 +48,51 @@ public class RegisterController {
     @FXML
     private TextField username;
 
+    UnaryOperator<TextFormatter.Change> filter = change -> {
+        String newText = change.getControlNewText();
+        if (Pattern.matches("[^0-9]*", newText)) {
+            return change; // Accept the change
+        } else {
+            return null; // Reject the change
+        }
+    };
+
+    public void initialize() {
+        TextFormatter<String> textFormatter = new TextFormatter<>(filter);
+        firstName.setTextFormatter(textFormatter);
+
+        TextFormatter<String> lastNameFormatter = new TextFormatter<>(filter);
+        lastName.setTextFormatter(lastNameFormatter);
+    }
     @FXML
     void register(ActionEvent event) {
-        // Implement registration logic
+        if(firstName.getText().length() <= 1){
+            Alert alert= new Alert(Alert.AlertType.INFORMATION);
+            alert.setContentText("First name must contain at least 2 characters");
+            alert.show();
+            return;
+        }
+        if(lastName.getText().length() <= 1){
+            Alert alert= new Alert(Alert.AlertType.INFORMATION);
+            alert.setContentText("Last name must contain at least 2 characters");
+            alert.show();
+            return;
+        }
+        if(password.getText().length() < 6){
+            Alert alert= new Alert(Alert.AlertType.INFORMATION);
+            alert.setContentText("Password must contain at least 6 characters");
+            alert.show();
+            return;
+        }
+        if(!password.getText().equals(confirmPassword.getText())){
+            Alert alert= new Alert(Alert.AlertType.INFORMATION);
+            alert.setContentText("Passwords are not the same");
+            alert.show();
+            return;
+        }
+        Alert alert= new Alert(Alert.AlertType.INFORMATION);
+        alert.setContentText("Successes");
+        alert.show();
     }
 
     @FXML
